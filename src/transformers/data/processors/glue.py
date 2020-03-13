403c92ab49bd69ca1d86f11d2e6664380676745b
Gary Liu <kll482@nyu.edu>
2020-03-13 01:14:04 -0400

    update glue

--- src/transformers/data/processors/glue.py
+++ src/transformers/data/processors/glue.py
@@ -551,6 +551,7 @@ class BoolqProcessor(DataProcessor):
             text_b = line["question"]
             label = line["label"]
             examples.append(InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+        print(line["passage"], "11111"*10)
         return examples
 
 
